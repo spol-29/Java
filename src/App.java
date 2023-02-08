5269e1d82ea19a3e786f1c0a29966d505f8b65e3
spol-29 <polineni.sriram9@gmail.com>
2023-02-07 18:26:18 -0600

    tested java

--- src/App.java
+++ src/App.java
@@ -3,11 +3,8 @@ class Main {
   public static void main(String[] args) {
     Scanner myObj = new Scanner(System.in);
     String userName;
-    
-    // Enter username and press Enter
     System.out.println("Enter username"); 
     userName = myObj.nextLine();   
-       
-    System.out.println("Username is: " + userName);        
+    System.out.println("Username is: " + userName);       
   }
 }
